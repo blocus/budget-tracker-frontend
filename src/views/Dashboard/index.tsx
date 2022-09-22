0094a8d42d0ee08e033224dd10b1a3514446da62
Ahmed Meftah <contact@ahmedmeftah.com>
2022-09-22 16:29:10 +0100

    Generate fake data

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,5 +1,40 @@
+import { expenseCategories, incomeCategories } from 'data'
+import { getDays } from 'helpers'
+import { useMemo } from 'react'
+import { PlannedAcutalValues } from 'types'
+import { MonthDataValue } from './MonthChart'
 function Dashboard() {
-  return null
+  const expenses: PlannedAcutalValues[] = useMemo(
+    () =>
+      expenseCategories.map(({ icon, title }) => ({
+        icon,
+        title,
+        planned: Math.random() * 1000,
+        actual: Math.random() * 1000,
+      })),
+    []
+  )
+
+  const incomes: PlannedAcutalValues[] = useMemo(
+    () =>
+      incomeCategories.map(({ icon, title }) => ({
+        icon,
+        title,
+        planned: Math.random() * 1000,
+        actual: Math.random() * 1000,
+      })),
+    []
+  )
+
+  const monthData: MonthDataValue[] = useMemo(
+    () =>
+      Array.from({ length: getDays(new Date().getMonth()) }).map((_, day) => ({
+        date: new Date(new Date().getFullYear(), new Date().getMonth(), day + 1),
+        income: Math.random() * 100,
+        expense: Math.random() * 100,
+      })),
+    []
+  )
 }
 
 export default Dashboard
