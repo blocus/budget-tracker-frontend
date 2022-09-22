46456fc8b52f822fa05d6daa18ba963be156cec6
Ahmed Meftah <contact@ahmedmeftah.com>
2022-09-22 16:26:44 +0100

    Init types and data

---
+++ src/data.ts
@@ -0,0 +1,32 @@
+import { CategoriesValuesInterface } from 'types'
+
+export const expenseCategories: CategoriesValuesInterface[] = [
+  { icon: '🏡', title: 'Rent' },
+  { icon: '🥝', title: 'Grocery' },
+  { icon: '🍔', title: 'Dining' },
+  { icon: '🚘', title: 'Transportation' },
+  { icon: '🛍', title: 'Shopping' },
+  { icon: '📚', title: 'Loan' },
+  { icon: '🐶', title: 'Pet' },
+  { icon: '⚡️', title: 'Utility' },
+  { icon: '💫', title: 'Personal' },
+  { icon: '🧷', title: 'Insurance' },
+  { icon: '📞', title: 'Phone' },
+  { icon: '💪', title: 'Gym' },
+  { icon: '🧘‍♂️', title: 'Wellness' },
+  { icon: '✈️', title: 'Travel' },
+  { icon: '🏡', title: 'Home Good' },
+  { icon: '👩‍⚕️', title: 'Medical' },
+  { icon: '🎗', title: 'Giving' },
+  { icon: '🍿', title: 'Entertainment' },
+  { icon: '💵', title: 'Interest' },
+  { icon: '🙉', title: 'Other' },
+]
+
+export const incomeCategories: CategoriesValuesInterface[] = [
+  { icon: '🔨', title: 'Work' },
+  { icon: '🚀', title: 'Freelance' },
+  { icon: '💸', title: 'Dividantes' },
+]
+
+export const currency = 'TND'
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface PlannedAcutalValues {
+  icon: string
+  title: string
+  planned: number
+  actual: number
+}
+
+export interface CategoriesValuesInterface {
+  icon: string
+  title: string
+}
